6db07fdbca1ed358e9775af5c7b6f5e759233468
ali safi <ali800safi@gmail.com>
2025-06-28 18:29:53 +0330

    first commit

---
+++ medrick-game-server/Network/Transport.cs
@@ -0,0 +1,14 @@
+﻿namespace medrick_game_server.Network;
+
+public interface Transport
+{
+    Task<ConnectionResult> Connect();
+    Task Disconnect();
+    void SendMessage(byte[] messageToBytes);
+    void RegisterReceivedMessage(Action<byte[]> OnRecievedMessage);
+}
+
+public class ConnectionResult
+{
+    public int NetworkId;
+}
---
+++ medrick-game-server/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace medrick_game_server;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
