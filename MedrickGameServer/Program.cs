0a43c74706d88cf89301c6c3a458067d0998feac
Aryana <aryana761227@gmail.com>
2025-06-28 19:58:00 +0330

    Start server  from main.

--- MedrickGameServer/Program.cs
+++ MedrickGameServer/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace MedrickGameServer;
+﻿using MedrickGameServer.Network.Application;
+using MedrickGameServer.NetworkLayer.Main;
+
+namespace MedrickGameServer;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        NetworkFactory networkFactory = new LiteNetLibFactory();
+        NetworkServer networkServer = networkFactory.CreateServer();
+        await networkServer.StartAsync(9050);
     }
 }
